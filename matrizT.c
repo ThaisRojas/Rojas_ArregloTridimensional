ef038f308faccb49c6e2b4589fff4af103e21517
Thais Rojas <thais.rojas@udla.edu.ec>
2023-11-30 16:32:55 -0500

    Matrices en 0

--- matrizT.c
+++ matrizT.c
@@ -7,4 +7,14 @@ int main() {
     printf("Ingrese las dimensiones (x, y, z) del arreglo tridimensional separadas por espacios: "); // Pide al usuario las dimensiones
     scanf("%d %d %d", &x, &y, &z);
 
+
+    int arreglo[x][y][z]; // Declara el arreglo tridimensional con las dimensiones
+
+    for (int i = 0; i < x; i++) { // Inicia las matrices en ceros
+        for (int j = 0; j < y; j++) {
+            for (int k = 0; k < z; k++) {
+                arreglo[i][j][k] = 0;
+            }
+        }
+    }
 }
