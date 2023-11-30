3c70e5e4d72bbf81babf33c32b1b5d327402a653
Thais Rojas <thais.rojas@udla.edu.ec>
2023-11-30 16:22:40 -0500

    Pide ingresar las dimensiones

---
+++ matrizT.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int x, y, z;
+
+
+    printf("Ingrese las dimensiones (x, y, z) del arreglo tridimensional separadas por espacios: "); // Pide al usuario las dimensiones
+    scanf("%d %d %d", &x, &y, &z);
+
+}
